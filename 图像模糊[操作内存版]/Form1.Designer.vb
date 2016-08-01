be2c997a81338a85ed6378d1a6d636be6407d8cc
Leon <xiaoyankeji@hotmail.com>
2016-08-01 18:54:54 +0800

    第一次提交

---
+++ 图像模糊[操作内存版]/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Test4 = New System.Windows.Forms.Button()
+        Me.Test3 = New System.Windows.Forms.Button()
+        Me.Test2 = New System.Windows.Forms.Button()
+        Me.Test1 = New System.Windows.Forms.Button()
+        Me.SetPixel = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SetPixel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(1, 1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(540, 360)
+        Me.PictureBox1.TabIndex = 14
+        Me.PictureBox1.TabStop = False
+        '
+        'Test4
+        '
+        Me.Test4.ForeColor = System.Drawing.Color.Red
+        Me.Test4.Image = CType(resources.GetObject("Test4.Image"), System.Drawing.Image)
+        Me.Test4.Location = New System.Drawing.Point(547, 129)
+        Me.Test4.Name = "Test4"
+        Me.Test4.Size = New System.Drawing.Size(75, 33)
+        Me.Test4.TabIndex = 13
+        Me.Test4.Text = "Test-4"
+        Me.Test4.UseVisualStyleBackColor = True
+        '
+        'Test3
+        '
+        Me.Test3.ForeColor = System.Drawing.Color.Red
+        Me.Test3.Image = CType(resources.GetObject("Test3.Image"), System.Drawing.Image)
+        Me.Test3.Location = New System.Drawing.Point(547, 90)
+        Me.Test3.Name = "Test3"
+        Me.Test3.Size = New System.Drawing.Size(75, 33)
+        Me.Test3.TabIndex = 12
+        Me.Test3.Text = "Test-3"
+        Me.Test3.UseVisualStyleBackColor = True
+        '
+        'Test2
+        '
+        Me.Test2.ForeColor = System.Drawing.Color.Red
+        Me.Test2.Image = CType(resources.GetObject("Test2.Image"), System.Drawing.Image)
+        Me.Test2.Location = New System.Drawing.Point(547, 51)
+        Me.Test2.Name = "Test2"
+        Me.Test2.Size = New System.Drawing.Size(75, 33)
+        Me.Test2.TabIndex = 11
+        Me.Test2.Text = "Test-2"
+        Me.Test2.UseVisualStyleBackColor = True
+        '
+        'Test1
+        '
+        Me.Test1.ForeColor = System.Drawing.Color.Red
+        Me.Test1.Image = CType(resources.GetObject("Test1.Image"), System.Drawing.Image)
+        Me.Test1.Location = New System.Drawing.Point(547, 12)
+        Me.Test1.Name = "Test1"
+        Me.Test1.Size = New System.Drawing.Size(75, 33)
+        Me.Test1.TabIndex = 10
+        Me.Test1.Text = "Test-1"
+        Me.Test1.UseVisualStyleBackColor = True
+        '
+        'SetPixel
+        '
+        Me.SetPixel.Location = New System.Drawing.Point(549, 195)
+        Me.SetPixel.Name = "SetPixel"
+        Me.SetPixel.Size = New System.Drawing.Size(58, 21)
+        Me.SetPixel.TabIndex = 16
+        Me.SetPixel.Value = New Decimal(New Integer() {2, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(549, 180)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 12)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "模糊半径："
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(547, 242)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 300
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(626, 362)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.SetPixel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Test4)
+        Me.Controls.Add(Me.Test3)
+        Me.Controls.Add(Me.Test2)
+        Me.Controls.Add(Me.Test1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "图像模糊[操作内存版]"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SetPixel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Test4 As Button
+    Friend WithEvents Test3 As Button
+    Friend WithEvents Test2 As Button
+    Friend WithEvents Test1 As Button
+    Friend WithEvents SetPixel As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ 图像模糊[操作内存版]/Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+    Private Sub TestButton_Click(sender As Object, e As EventArgs) Handles Test1.Click, Test2.Click, Test3.Click, Test4.Click
+        Dim D As Integer = My.Computer.Clock.TickCount
+
+        PictureBox1.Image = CType(sender, Button).Image
+        Dim TempPicture As Bitmap = CType(sender, Button).Image
+        PictureBox1.Image = 模糊图像(TempPicture, SetPixel.Value)
+
+        '显示处理事件耗时
+        MsgBox("Task finished!" & vbCrLf & "共用时： 【" & (My.Computer.Clock.TickCount - D) / 1000 & "】 秒。", MsgBoxStyle.Information)
+    End Sub
+
+    Private Function 模糊图像(ByVal InitialBitmap As Bitmap, ByVal Radius As Integer) As Bitmap
+        Dim ResualtBitmap As Bitmap = New Bitmap(InitialBitmap)
+        Dim ResualtBitmapData As Imaging.BitmapData = New Imaging.BitmapData
+        ResualtBitmapData = ResualtBitmap.LockBits(New Rectangle(0, 0, InitialBitmap.Width, InitialBitmap.Height), Imaging.ImageLockMode.WriteOnly, ResualtBitmap.PixelFormat)
+        Dim DoubleRadius As Integer = Radius * 2
+        Dim DataStride As Integer = ResualtBitmapData.Stride
+        Dim DataWidth As Integer = ResualtBitmapData.Width
+        Dim DataHeight As Integer = ResualtBitmapData.Height
+        Dim InitalDataArray(DataStride * DataHeight - 1) As Byte
+        Dim DataArray(DataStride * DataHeight - 1) As Byte
+        Dim Position(DoubleRadius, DoubleRadius) As Integer
+        Runtime.InteropServices.Marshal.Copy(ResualtBitmapData.Scan0, InitalDataArray, 0, InitalDataArray.Length)
+        Dim Index = 0, IndexX, IndexY As Integer, Round, RoundX, RoundY As Integer
+        Dim ByteSum, AValue, RValue, GValue, BValue As Integer
+        Dim Boundary(DataHeight) As Integer, LineIndex As Integer
+        For RoundY = 0 To DoubleRadius
+            Index = (RoundY - Radius) * DataWidth - Radius
+            For RoundX = 0 To DoubleRadius
+                Position(RoundY, RoundX) = IIf((RoundX - Radius) ^ 2 + (RoundY - Radius) ^ 2 <= Radius ^ 2, 4 * (Index + RoundX), 0)
+            Next
+        Next
+        For IndexY = 0 To DataHeight - 1
+            Boundary(IndexY + 1) = Boundary(IndexY) + DataStride
+        Next
+        For IndexY = 0 To DataHeight - 1
+            For IndexX = 0 To DataWidth - 1
+                ByteSum = 0 : AValue = 0 : RValue = 0 : GValue = 0 : BValue = 0
+                Index = IndexY * DataStride + IndexX * 4
+                For RoundY = 0 To DoubleRadius
+                    LineIndex = IndexY + RoundY - Radius
+                    If 0 <= LineIndex AndAlso LineIndex < Boundary.Count - 1 Then
+                        For RoundX = 0 To DoubleRadius
+                            Round = Index + Position(RoundY, RoundX)
+                            If Boundary(LineIndex) <= Round AndAlso Round < Boundary(LineIndex + 1) Then
+                                BValue += InitalDataArray(Round)
+                                GValue += InitalDataArray(Round + 1)
+                                RValue += InitalDataArray(Round + 2)
+                                AValue += InitalDataArray(Round + 3)
+                                ByteSum += 1
+                            End If
+                        Next
+                    End If
+                Next
+                AValue /= ByteSum : RValue /= ByteSum : GValue /= ByteSum : BValue /= ByteSum
+                DataArray(Index) = BValue
+                DataArray(Index + 1) = GValue
+                DataArray(Index + 2) = RValue
+                DataArray(Index + 3) = AValue
+            Next
+        Next
+        Runtime.InteropServices.Marshal.Copy(DataArray, 0, ResualtBitmapData.Scan0, DataArray.Length)
+        ResualtBitmap.UnlockBits(ResualtBitmapData)
+        Return ResualtBitmap
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim B As Bitmap = New Bitmap(Me.Width, Me.Height)
+        Dim G As Graphics = Graphics.FromImage(B)
+        G.CopyFromScreen(Me.Location, New Point(0, 0), Me.Size)
+        PictureBox1.Image = B
+        Me.Show()
+        Me.Refresh()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Me.Hide()
+        Me.Opacity = 0
+        Application.DoEvents()
+        Dim B As Bitmap = New Bitmap(Me.Width, Me.Height)
+        Dim G As Graphics = Graphics.FromImage(B)
+        G.CopyFromScreen(Me.Location, New Point(0, 0), Me.Size)
+        PictureBox1.Image = 模糊图像(B, SetPixel.Value)
+        Me.Opacity = 1
+        'Me.Show()
+        Me.Refresh()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Static B As Bitmap
+        B = New Bitmap(Me.Width, Me.Height)
+        Static G As Graphics
+        G = Graphics.FromImage(B)
+        G.CopyFromScreen(New Point(0, 0), New Point(0, 0), Me.PictureBox1.Size)
+        PictureBox1.Image = 模糊图像(B, SetPixel.Value)
+    End Sub
+End Class
